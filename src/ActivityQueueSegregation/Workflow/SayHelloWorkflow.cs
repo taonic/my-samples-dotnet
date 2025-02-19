e7b30f5779a4f074ed94278e63247bb25f102a26
Tao Guo <tao@temporal.io>
2025-02-19 14:11:42 +1100

    Show case how to reference activity by its method name

--- src/ActivityQueueSegregation/Workflow/SayHelloWorkflow.cs
+++ src/ActivityQueueSegregation/Workflow/SayHelloWorkflow.cs
@@ -3,7 +3,6 @@ namespace TemporalioSamples.ActivityQueueSegregation.Workflow;
 using Microsoft.Extensions.Logging;
 using Temporalio.Workflows;
 using TemporalioSamples.ActivityQueueSegregation.ActivityWorker1;
-using TemporalioSamples.ActivityQueueSegregation.ActivityWorker2;
 
 [Workflow]
 public class SayHelloWorkflow
@@ -22,12 +21,13 @@ public class SayHelloWorkflow
         Workflow.Logger.LogInformation("Activity static method result: {Result}", result1);
 
         // Run an async instance method activity in it's own task queue.
-        var result2 = await Workflow.ExecuteActivityAsync(
-            (ActivityWorker2.InstanceActivities act) => act.SelectFromDatabaseAsync("some-db-table"),
+        var result2 = await Workflow.ExecuteActivityAsync<string>(
+            "SelectFromDatabase", // Alternatively, you can reference the Activity by its method name.
+            new object?[] { "some-db-table" },
             new()
             {
                 StartToCloseTimeout = TimeSpan.FromSeconds(10),
-                TaskQueue = ActivityWorker2.Worker.TaskQueue,
+                TaskQueue = "my-activity-task-queue-2",
             });
         Workflow.Logger.LogInformation("Activity instance method result: {Result}", result2);
 
